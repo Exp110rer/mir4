347379a5a9d0a38d7909b40f85d471d5b0dbf91a
Exp110rer <73857939+Exp110rer@users.noreply.github.com>
2023-03-30 22:40:55 +0300

    sprint_9: add productCategory and SimpleRouter instead of DefaultRouter (#10)
    Co-authored-by: Alexander Sakharov <Alexander_Sakharov@bat.com>

--- hub_api/urls.py
+++ hub_api/urls.py
@@ -2,19 +2,19 @@ from django.urls import path, include
 from hub_api.apps import HubApiConfig
 from hub_api.viewsets import OrderCreateModelViewSetSNS, OrderRetrieveModelViewSet, OrderCreateNonTNTModelViewSet, \
     OrderForSputnikListViewSet, OrderForSputnikRetrieveViewSet, OrderForSputnikUUIDUpdateViewSet, OrderForSputnikCodeValidityUpdateViewSet
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
 from rest_framework_simplejwt.views import TokenObtainSlidingView
 
 app_name = HubApiConfig.name
 
-router_hub_api = DefaultRouter()
+router_hub_api = SimpleRouter()
 router_hub_api.register('sOrder', OrderCreateModelViewSetSNS)
 router_hub_api.register('gOrder', OrderRetrieveModelViewSet)
 router_hub_api.register('sOrderNonTnt', OrderCreateNonTNTModelViewSet)
 
-router_hub_api_validation = DefaultRouter()
+router_hub_api_validation = SimpleRouter()
 router_hub_api_validation.register('list', OrderForSputnikListViewSet)
 router_hub_api_validation.register('uuidupdate', OrderForSputnikUUIDUpdateViewSet)
 router_hub_api_validation.register('retrieve', OrderForSputnikRetrieveViewSet)
