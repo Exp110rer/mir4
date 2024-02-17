6094bbe821da4644a47dcff63ec00ba4077889f4
Exp110rer <73857939+Exp110rer@users.noreply.github.com>
2024-02-17 21:08:07 +0300

    sprint_25: bcp portal (#25)
    Co-authored-by: Aleksandr Sakharov <Alexander_Sakharov@itms.ru>

--- mirusers/views.py
+++ mirusers/views.py
@@ -12,7 +12,7 @@ class MirUserLoginView(LoginView):
 
     def get_success_url(self):
         if self.request.user.groups.filter(name='Orders_BCP').exists():
-            return reverse_lazy('hub_portal:orders_cs')
+            return reverse_lazy('hub_portal:orders_bcp')
         elif self.request.user.groups.filter(name='Orders_CS').exists():
             return reverse_lazy('hub_portal:orders_cs')
         else:
